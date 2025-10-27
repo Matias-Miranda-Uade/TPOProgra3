ec124749850127af4911d181fcaa6612892ace8b
Matias-Miranda-Uade <mamiranda@uade.edu.ar>
2025-10-27 17:22:41 -0300

    Delete src directory

--- src/main/java/com/example/demo/repository/RoadRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-
-import com.example.demo.model.RoadRelationship;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-
-public interface RoadRepository extends Neo4jRepository<RoadRelationship, Integer> {
-}
