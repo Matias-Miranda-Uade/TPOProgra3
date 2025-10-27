e5f007b999717f65b79eb5cf4fa69bb436f2c3ec
Matias-Miranda-Uade <mamiranda@uade.edu.ar>
2025-10-27 17:21:08 -0300

    Delete TPO Progra 3 directory

--- TPO Progra 3/src/main/java/com/example/demo/repository/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.MovieEntity;
-import org.springframework.data.neo4j.repository.ReactiveNeo4jRepository;
-import reactor.core.publisher.Mono;
-
-public interface MovieRepository extends ReactiveNeo4jRepository<MovieEntity,String> {
-    Mono<MovieEntity> findOneByTitle(String title);
-}
